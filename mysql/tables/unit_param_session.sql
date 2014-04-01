ce490eee50d969155f0a1ba8a77b4d59da6efc46
morten <mortensimon@gmail.com>
2014-04-01 07:51:53 +0200

    Fixed a bug - caused the SQL not to load (a comma too many)

--- mysql/tables/unit_param_session.sql
+++ mysql/tables/unit_param_session.sql
@@ -6,7 +6,7 @@ CREATE TABLE `unit_param_session` (
   PRIMARY KEY (`unit_id`, `unit_type_param_id`),
   CONSTRAINT `fk_unit_param_session_unit_id` FOREIGN KEY `fk_unit_param_session_unit_id` (`unit_id`)
     REFERENCES `unit` (`unit_id`)
-    ON DELETE CASCADE,
+    ON DELETE CASCADE
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_unit_param_session_u_t_p_id` FOREIGN KEY `fk_unit_param_session_u_t_p_id` (`unit_type_param_id`)
     REFERENCES `unit_type_param` (`unit_type_param_id`)
